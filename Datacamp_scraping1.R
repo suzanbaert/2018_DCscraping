f1b84a6d9e464cf9e9db4fd1bdf1bc52f2ef0486
suzan baert <suzan.baert@outlook.com>
2018-04-16 20:24:16 +0200

    main page links

--- Datacamp_scraping1.R
+++ Datacamp_scraping1.R
@@ -1,4 +1,5 @@
 library(rvest)
+library(xml2)
 
 robotstxt::paths_allowed("https://www.datacamp.com/courses/tech:r")
 
@@ -8,33 +9,34 @@ robotstxt::paths_allowed("https://www.datacamp.com/courses/tech:r")
 url <- "https://www.datacamp.com/courses/tech:r"
 
 
-
+#main_page_links ####
 
 # write the javascript code to a new scrape.js file
 writeLines("var webPage = require('webpage');
            var page = webPage.create();
            
            var fs = require('fs');
-           var path = 'phantomJS_pages/mainpage.html'
+           var path = 'phantomJS_htmlpages/mainpage.html'
            
            page.open('https://www.datacamp.com/courses/tech:r', function (status) {
            var content = page.content;
            fs.write(path,content,'w')
            phantom.exit();});",
            
-           con = "phantomJS_calls/scrape_mainpage2.js")
-
-
-
+           con = "phantomJS_scripts/scrape_mainpage2.js")
 
 
 # Download website via system call
 # first argument location of phantomjs, then location of scraping script
-system("E:///phantomjs/bin/phantomjs phantomJS_calls/scrape_mainpage2.js")
+system("E:///phantomjs/bin/phantomjs phantomJS_scripts/scrape_mainpage2.js")
 
 
+#scraping links
+read_html("phantomJS_htmlpages/mainpage.html")
 
-
+all_r_links <- XML::getHTMLLinks("phantomJS_htmlpages/mainpage.html")
+all_r_links <- all_r_links[59:214]
+all_r_links <- all_r_links[duplicated(all_r_links)]
 
 
 
